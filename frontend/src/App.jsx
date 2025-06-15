43684fa05272aa8aa8092853d3e731bbeeb16083
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-06-15 20:36:20 +0100

    ADD: App routes for Home, Create and View Notes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreateNote from './pages/CreateNote'
+import ViewNote from './pages/ViewNote'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      
+      <Routes>        
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateNote />} />
+        <Route path="/view/:id" element={<ViewNote />} />        
+      </Routes>
+
+    </div>
   )
 }
 
