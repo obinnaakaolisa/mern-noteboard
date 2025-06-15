5bb2cf2e22c9e54db9710b3c9912fe73419057a3
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-06-15 20:42:23 +0100

    UPDATE: Test the react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreateNote from './pages/CreateNote'
 import ViewNote from './pages/ViewNote'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
-      
-      <Routes>        
+    <div>      
+      <button onClick={ () => toast.success("Congratulations!")}>Click Me</button>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateNote />} />
         <Route path="/view/:id" element={<ViewNote />} />        
