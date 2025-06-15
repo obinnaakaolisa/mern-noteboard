52292f972f6c3160cb545f82798552fe5253e7a6
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-06-15 20:56:06 +0100

    ADD: TailwindCss config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,15 @@ import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreateNote from './pages/CreateNote'
 import ViewNote from './pages/ViewNote'
-import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>      
-      <button onClick={ () => toast.success("Congratulations!")}>Click Me</button>
+    <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateNote />} />
         <Route path="/view/:id" element={<ViewNote />} />        
       </Routes>
-
     </div>
   )
 }
