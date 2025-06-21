336f756d9cb7c30f079557becd4bcd138bb0886d
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-06-21 12:17:46 +0100

    UPDATE: CreateNote page and logic

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <div className="flex items-center justify-between">
                 <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>Noteboard</h1>
                 <div className="flex items-center gap-4">
-                    <Link to={"/create"} className="btn btn-primary">
+                    <Link to={"/notes/create"} className="btn btn-primary">
                         <PlusIcon className='size-5' />
                         <span>Add a Note</span>
                     </Link>
--- frontend/src/pages/CreateNote.jsx
+++ frontend/src/pages/CreateNote.jsx
@@ -1,8 +1,112 @@
-import React from 'react'
+import { ArrowLeftIcon } from "lucide-react";
+import { Link, useNavigate } from "react-router";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const CreateNote = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    // Prevent default form submission
+    e.preventDefault();
+    // Validate input fields
+    let trimmedTitle = title.trim();
+    let trimmedContent = content.trim();
+    
+    if (!trimmedTitle || !trimmedContent ) {
+      toast.error("All input fields are required.");
+      return;
+    }
+
+    // Set loading state to true
+    setLoading(true);
+
+    // Simulate an API call to create a note
+    try {
+      await axios.post("http://localhost:2000/api/notes", {
+        title: trimmedTitle,
+        content: trimmedContent,
+      });
+
+      // Show success message
+      toast.success("Note created successfully!");
+
+      navigate("/");
+
+    } catch (error) {
+      console.error("Error creating note:", error);
+      if (error.response.status === 429) {
+        toast.error("Too many requests. Please try again later.", {
+          duration: 4000,
+          icon: "⚠️",
+        });
+      } else {
+        toast.error("Failed to create note. Please try again.");
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>CreateNote</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="mr-2 size-5 " />
+            Back to Home
+          </Link>
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title text-2xl font-bold mb-4">Create a New Note</h2>
+              <form action="POST" onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label mb-2">
+                    <span className="label-text">Title:</span>
+                  </label>
+                  <input 
+                    type="text" 
+                    placeholder="Enter note title" 
+                    id="title"
+                    className="input input-bordered w-full"
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label mb-2">
+                    <span className="label-text">Content:</span>
+                  </label>
+                  <textarea 
+                    placeholder="Write your note here..." 
+                    id="content"
+                    className="textarea textarea-bordered w-full h-48"
+                    value={content} 
+                    onChange={(e) => setContent(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="card-actions justify-end">
+                  <button 
+                    type="submit" 
+                    className={`btn btn-primary mt-4 ${loading ? 'loading' : ''}`}
+                    onClick={handleSubmit}
+                    disabled={loading}
+                  >
+                    {loading ? 'Creating...' : 'Create Note'}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
