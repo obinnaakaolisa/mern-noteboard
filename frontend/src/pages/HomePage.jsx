68f1df8efb6696ea49f6bce2c08a0dff7b810b14
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-06-21 13:19:55 +0100

    ADD: Empty state UI for empty notes arrray

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-hot-toast';
 import Navbar from '../components/Navbar';
 import RateLimitAlert from '../components/RateLimitAlert';
 import NoteCard from '../components/NoteCard';
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -38,6 +39,7 @@ const HomePage = () => {
       {isRateLimited && <RateLimitAlert />}
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         { loading && <div className="text-center text-primary py-10">Loading notes ...</div> }
+        { notes.length === 0 && !loading && !isRateLimited && ( <NotesNotFound />) }
         { notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map(note => (
